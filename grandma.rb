9735bcf9fb8e489054789a56cdad555474cbbc45
Zach  Smith <zdsmith001@gmail.com>
2019-07-12 17:37:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string == !string.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
+  if string == string.upcase
+    puts "NO, NOT SINCE 1938!"
   end
 end
 # Whatever you say to grandma, she should respond with

13df8eeab320920a91e378c943aaeda93396c277
Zach  Smith <zdsmith001@gmail.com>
2019-07-12 17:44:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase = phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  elsif phrase = "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end

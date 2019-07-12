4fb7cc0fb309d37a7ebb3af3b927430027cec6e9
Zach  Smith <zdsmith001@gmail.com>
2019-07-12 17:31:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(text)
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif text.lowercase
     puts "HUH?! SPEAK UP SONNY!"
-  else 
+  else
     puts "NO, NOT SINCE 1938!"
   end
 end
@@ -12,7 +12,7 @@ end
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!

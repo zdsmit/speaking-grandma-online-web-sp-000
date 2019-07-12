0034b53662b90e6a94325befbb030af4a360a302
Zach  Smith <zdsmith001@gmail.com>
2019-07-12 17:40:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma("")
-  if string == string.upcase
+def speak_to_grandma(phrase)
+  if phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
   end
 end

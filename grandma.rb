735621786e608b9e94aea5e7f2d19b61504e016e
Zach  Smith <zdsmith001@gmail.com>
2019-07-12 17:36:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string = "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+  if string == !string.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
   end
 end
 # Whatever you say to grandma, she should respond with
